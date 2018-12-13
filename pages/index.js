9767e923035e98b91607b09d039db022dd4387a8
Zac Holland <zacharyholland@gmail.com>
2018-12-12 18:15:50 -0800

    init commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello Next.js</p>
+      </div>
+    );
+  }
+}
+
+export default Index;
